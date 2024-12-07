4a4a364b38b085e9432e875789cb31150cce1e06
Noah Lehmann <noah.lehmann@protonmail.com>
2024-12-07 23:42:38 +0100

    add inspiration and title

--- src/components/footer/Footer.svelte
+++ src/components/footer/Footer.svelte
@@ -14,6 +14,10 @@
 
     <div class="legal">
         <p>All Rights Reserved 2024 - Noah Lehmann</p>
+        <a class="inspiration"
+           href="https://en.wikipedia.org/wiki/The_Pragmatic_Programmer">
+            Inspired by the ideas mentioned in <em>The Pragmatic Programmer</em>
+        </a>
     </div>
 </div>
 
@@ -45,5 +49,17 @@
         justify-content: space-between;
         align-items: center;
         padding-top: 2%;
+    }
+
+    .legal {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        gap: 0.2rem;
+    }
+
+    .inspiration {
+        color: #777777;
+        font-size: 0.7rem;
     }
 </style>
